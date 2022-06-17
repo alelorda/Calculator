66cc4e87688512c7752c1228bc714cc4d4cb95e3
Ariel Alexis Lorda Barrabasqui <alexis.lorda@gmail.com>
2022-06-17 18:05:11 -0300

    remove <div>

--- calculator/src/Calculator.tsx
+++ calculator/src/Calculator.tsx
@@ -1,26 +1,3 @@
-/* import React from 'react';
-import './App.css';
-
-
-function App() {
-  return (
-    <div classNameName="App-header">
-      <div classNameName="App">
-        <header >
-          <h1> Calculator</h1>
-        </header>
-        <Button variant='contained'>*</Button>
-        <Button variant='contained'>-</Button>
-        <Button variant='contained'>/</Button>
-        <Button variant='contained'>+</Button>
-        <Button variant='contained'>AC</Button>
-        <Button variant='contained'>=</Button>
-      </div>
-    </div>
-  );
-}
-*/
-
 import react, { useState } from "react";
 import "./App.css";
 import { Button } from '@material-ui/core';
@@ -54,64 +31,62 @@ function App() {
   };
 
   return (
-    <div className="App-header">
-      <div className="container">
-        <h1 className="title">Calculator</h1>
-        <div className="calculator">
-          <input type="text" className="calc-numbers" value={result} />
-          <div className="calculator-buttons">
-            <Button onClick={clear} className="btn clear span-2">
-              C
-            </Button>
-            <Button onClick={backSpace} className="btn orange ">
-              &larr;
-            </Button>
-            <Button onClick={handleClick} name="/" className="btn orange ">
-              &divide;
-            </Button>
-            <Button onClick={handleClick} name="7" className="btn">
-              7
-            </Button>
-            <Button onClick={handleClick} name="8" className="btn">
-              8
-            </Button>
-            <Button onClick={handleClick} name="6" className="btn">
-              9
-            </Button>
-            <Button onClick={handleClick} name="*" className="btn orange">
-              x
-            </Button>
-            <Button onClick={handleClick} name="4" className="btn">
-              4
-            </Button>
-            <Button onClick={handleClick} name="5" className="btn">
-              5
-            </Button>
-            <Button onClick={handleClick} name="6" className="btn">
-              6
-            </Button>
-            <Button onClick={handleClick} name="-" className="btn">
-              -
-            </Button>
-            <Button onClick={handleClick} name="1" className="btn">
-              1
-            </Button>
-            <Button onClick={handleClick} name="2" className="btn">
-              2
-            </Button>
-            <Button onClick={handleClick} name="3" className="btn">
-              3
-            </Button>
-            <Button onClick={handleClick} name="+" className="btn orange">
-              +
-            </Button>
-            <Button onClick={handleClick} name="0" className="btn span-3">
-              0
-            </Button>
-            <Button onClick={calc} name="=" className="btn orange  equal">
-              =
-            </Button>
-          </div>
+    <div className="container">
+      <h1 className="title">Calculator</h1>
+      <div className="calculator">
+        <input type="text" className="calc-numbers" value={result} />
+        <div className="calculator-buttons">
+          <Button onClick={clear} className="btn clear span-2">
+            C
+          </Button>
+          <Button onClick={backSpace} className="btn orange ">
+            &larr;
+          </Button>
+          <Button onClick={handleClick} name="/" className="btn orange ">
+            &divide;
+          </Button>
+          <Button onClick={handleClick} name="7" className="btn">
+            7
+          </Button>
+          <Button onClick={handleClick} name="8" className="btn">
+            8
+          </Button>
+          <Button onClick={handleClick} name="6" className="btn">
+            9
+          </Button>
+          <Button onClick={handleClick} name="*" className="btn orange">
+            x
+          </Button>
+          <Button onClick={handleClick} name="4" className="btn">
+            4
+          </Button>
+          <Button onClick={handleClick} name="5" className="btn">
+            5
+          </Button>
+          <Button onClick={handleClick} name="6" className="btn">
+            6
+          </Button>
+          <Button onClick={handleClick} name="-" className="btn">
+            -
+          </Button>
+          <Button onClick={handleClick} name="1" className="btn">
+            1
+          </Button>
+          <Button onClick={handleClick} name="2" className="btn">
+            2
+          </Button>
+          <Button onClick={handleClick} name="3" className="btn">
+            3
+          </Button>
+          <Button onClick={handleClick} name="+" className="btn orange">
+            +
+          </Button>
+          <Button onClick={handleClick} name="0" className="btn span-3">
+            0
+          </Button>
+          <Button onClick={calc} name="=" className="btn orange  equal">
+            =
+          </Button>
         </div>
       </div>
     </div>
